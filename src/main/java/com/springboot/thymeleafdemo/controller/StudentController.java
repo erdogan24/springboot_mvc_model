42ee564cf7bdc95f4b18fbc6a70682b5039e7a2b
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-04 01:29:53 +0300

    Student controller has added

--- src/main/java/com/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/springboot/thymeleafdemo/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -23,4 +25,18 @@ public class StudentController {
         return "student-form";
 
     }
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student thestudent) {
+
+        // log the input data
+        System.out.println("theStudent: " + thestudent.getFirstName() + " " + thestudent.getLastName());
+
+        return "student-confirmation";
+    }
+
 }
+
+
+
+
+
